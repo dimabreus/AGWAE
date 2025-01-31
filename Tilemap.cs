c1c3211619abab25f250e29ceac81fccb641db10
dimabreus <dmitriybreus5@gmail.com>
2025-02-01 01:09:26 +0300

    Added export/import functions for tilemap

--- Tilemap.cs
+++ Tilemap.cs
@@ -1,10 +1,12 @@
 ﻿using System.Numerics;
+using System.Text.Json;
 
 namespace AGWAE
 {
     internal class Tilemap(Func<GameObject>[] gameObjectVariants, Vector2 position)
     {
-        public Dictionary<Vector2, GameObject> GameObjects { get; private set; } = new();
+        public Dictionary<Vector2, GameObject> GameObjects { get; private set; } = [];
+        private readonly Dictionary<Vector2, int> Grid = [];
 
         private readonly Func<GameObject>[] gameObjectVariants = gameObjectVariants;
         private Vector2 position = position;
@@ -12,6 +14,21 @@ namespace AGWAE
         public Tilemap(Func<GameObject>[] gameObjectVariants)
             : this(gameObjectVariants, Vector2.Zero)
         {
+        }
+
+        public Tilemap(Func<GameObject>[] gameObjectVariants, string filepath)
+            : this(gameObjectVariants)
+        {
+            LoadGridFromFile(filepath);
+
+            foreach (var el in Grid)
+            {
+                GameObject gameObject = gameObjectVariants[el.Value]();
+                gameObject.Position = position + el.Key * gameObject.Size;
+
+                GameObjects[el.Key] = gameObject;
+            }
+
         }
 
         public void SetAt(int x, int y, int gameObjectVariant)
@@ -24,6 +41,37 @@ namespace AGWAE
             gameObject.Position = position + new Vector2(x, y) * gameObject.Size;
 
             GameObjects[key] = gameObject;
+            Grid[key] = gameObjectVariant;
+        }
+
+        public void Export(string filePath)
+        {
+            var serializedGrid = new Dictionary<string, int>();
+            foreach (var kvp in Grid)
+            {
+                serializedGrid[$"({kvp.Key.X},{kvp.Key.Y})"] = kvp.Value;
+            }
+
+            JsonSerializerOptions options = new() { WriteIndented = true };
+            string json = JsonSerializer.Serialize(serializedGrid, options);
+            File.WriteAllText(filePath, json);
+        }
+
+        private void LoadGridFromFile(string filePath)
+        {
+            string json = File.ReadAllText(filePath);
+            var serializedGrid = JsonSerializer.Deserialize<Dictionary<string, int>>(json);
+
+            foreach (var kvp in serializedGrid)
+            {
+                var parts = kvp.Key.Trim('(', ')').Split(',');
+                if (parts.Length == 2 &&
+                    float.TryParse(parts[0], out float x) &&
+                    float.TryParse(parts[1], out float y))
+                {
+                    Grid[new Vector2(x, y)] = kvp.Value;
+                }
+            }
         }
     }
 }
